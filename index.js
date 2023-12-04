eca45566e50e30ecc88f98a4f538468d2f25a43a
= <=>
2023-12-04 09:20:39 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+"use strict";
+//Your Own Array: Think of your favorite mode of transportation, such as a motorcycle or a car, and make a list that stores several examples. Use your list to print a series of statements about these items, such as “to oI would like wn a Honda motorcycle.”
+let favoriteTransportation = [];
+favoriteTransportation.push(["car", "honda"]);
+favoriteTransportation.push(["motorcycle", "honda"]);
+favoriteTransportation.push(["bicycle", "honda"]);
+//console.log(favoriteTransportation)
+//i would like to own aHonda motorcycle.
+// using for each loop to print the statements
+favoriteTransportation.forEach(([transport, brand]) => { console.log(`i would like to own a ${brand} ${transport}.`); });
